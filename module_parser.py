aa7e4198f383492450a2057ffc29330b0ea4eb5d
FlameHorizon <maciejpiotrowsk@gmail.com>
2020-05-03 11:30:09 +0200

    Fixed typos, added docstrings

--- module_parser.py
+++ module_parser.py
@@ -3,7 +3,7 @@ import module_doc
 
 class ModuleParser():
     """This parser is responsible for extracting information from raw VBA code
-    which later, can be used for generting markdown document."""
+    which later, can be used for generating markdown document."""
 
     def make(self, code):
         lines = code.split('\n')
@@ -26,29 +26,31 @@ class ModuleParser():
         return ln[name_start:open_parenthesis]
 
     def __get_args(self, ln):
-        open_paranthesis = ln.index('(')
-        close_paranthesis = ln.rindex(')')
+        open_parentheses = ln.index('(')
+        close_parentheses = ln.rindex(')')
 
-        if self.__no_args(open_paranthesis, close_paranthesis):
+        if self.__no_args(open_parentheses, close_parentheses):
             return ''
 
-        # Remove surrounding paranthesis
-        args = ln[open_paranthesis + 1: close_paranthesis]
+        # Remove surrounding parenthesis
+        args = ln[open_parentheses + 1: close_parentheses]
 
         args_list = args.split(', ')
         output = []
 
-        AS_KEYWORD_LENGTH = 4
-
+        AS_KEYWORD = ' As '
         for item in args_list:
-            as_keyword_start = item.index(" As ")
-            arg_type = item[as_keyword_start + AS_KEYWORD_LENGTH:]
+            as_keyword_start = item.index(AS_KEYWORD)
+            arg_type = item[as_keyword_start + len(AS_KEYWORD):]
             output.append(arg_type)
 
         return output
 
-    def __no_args(self, open_paranthesis, close_paranthesis):
-        return open_paranthesis + 1 == close_paranthesis
+    def __no_args(self, open_parentheses, close_parentheses):
+        return open_parentheses + 1 == close_parentheses
 
     def __remove(self, text, start_index, count):
+        """Returns a new string in which a specified number of characters in the current 
+        instance beginning at a specified position have been deleted."""
+
         return text[:start_index] + text[start_index + count:]
