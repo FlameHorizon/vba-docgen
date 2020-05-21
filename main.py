59b50855a3f992658dc0ed5393639ac421603473
FlameHorizon <maciejpiotrowsk@gmail.com>
2020-05-21 21:57:33 +0200

    Main will remove content of output folder is it exists

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import json
+import shutil
 import module_parser
 import method_parser
 
@@ -16,7 +17,15 @@ def get_modules(folder_path):
 
 
 def delete_folder_content(folder_path):
-    pass
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
 
 
 if __name__ == "__main__":
